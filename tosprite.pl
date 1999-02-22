705271c24d679a64783cb02fd5fdea328657afc3
mschilli <github@perlmeister.com>
1999-02-22 03:23:34 +0000

    new

---
+++ tosprite.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl -w
+
+use Net::FTP;
+
+$dir      = "HTML/devel";
+$host     = "sprite.netnation.com";
+
+$ftp = Net::FTP->new($host, Timeout => 60) || 
+    die "Cannot connect: $host";
+
+$ftp->login("perlmeis", "zztop2121") || die "Login failed";
+
+$ftp->cwd($dir) || die "Directory $dir doesn't exist";
+$ftp->binary();
+$ftp->put("Proc-Simple-1.13.tar.gz") || die "Cannot put";
+
+$ftp->quit();
