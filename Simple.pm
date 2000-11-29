147a410062148bbcaf09602c90c0f1070ec059eb
mschilli <github@perlmeister.com>
2000-11-29 05:24:37 +0000

    1.17

--- Simple.pm
+++ Simple.pm
@@ -102,7 +102,7 @@ require Exporter;
 
 @ISA     = qw(Exporter AutoLoader);
 @EXPORT  = qw( );
-$VERSION = '1.16';
+$VERSION = '1.17';
 
 ######################################################################
 # Globals: Debug and the mysterious waitpid nohang constant.
